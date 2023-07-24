d9f9a504753afb2b5b07a884de62e36aa90dc884
=MurungaOwen <=owenhood80@gmail.com>
2023-07-24 10:25:43 +0300

    media files resolve

--- elcz/settings.py
+++ elcz/settings.py
@@ -160,3 +160,10 @@ MESSAGE_TAGS = {
 import os
 MEDIA_URL="/media/"
 MEDIA_ROOT =os.path.join(BASE_DIR,"media")
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'hoodcodesapps@gmail.com'
+EMAIL_HOST_PASSWORD = '1@mh00d2'
