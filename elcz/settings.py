27f4f0c52b123749250a059456bafcd309b0ce5b
=MurungaOwen <=owenhood80@gmail.com>
2023-07-25 13:35:27 +0300

    debug false for deploy serving

--- elcz/settings.py
+++ elcz/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY =os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 
 ALLOWED_HOSTS = (['campus-ministry.onrender.com','127.0.0.1',
